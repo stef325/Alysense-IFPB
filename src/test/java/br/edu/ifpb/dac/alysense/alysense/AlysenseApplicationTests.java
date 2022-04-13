c5a4bb89557a3bd2e684596c4fa4547ffde5dc82
Bruna Stefany da Silva Reinaldo <202015020026@ad.ifpb.edu.br>
2022-04-13 19:10:57 -0300

    First Commit

---
+++ src/test/java/br/edu/ifpb/dac/alysense/alysense/AlysenseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.dac.alysense.alysense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlysenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
