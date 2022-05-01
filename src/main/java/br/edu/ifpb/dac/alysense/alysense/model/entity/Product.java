320f3c4806b4ee49b54476382d5261356b6e25a5
stef325 <brunastefany432@gmail.com>
2022-05-01 01:21:23 -0300

    Change relationship and type of an attribute

--- src/main/java/br/edu/ifpb/dac/alysense/alysense/model/entity/Product.java
+++ src/main/java/br/edu/ifpb/dac/alysense/alysense/model/entity/Product.java
@@ -1,12 +1,15 @@
 package br.edu.ifpb.dac.alysense.alysense.model.entity;
 
 import java.time.LocalDate;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.OneToMany;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -24,7 +27,7 @@ public class Product {
     private LocalDate expirationDate;
     private String owner;
 
-    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
-    private Characteristic characteristic;
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Characteristic> characteristic;
     
 }
