2ce7b4734eede216963f9b272b7c111839e4d1d8
FilipeOBarbosa <filipeoliver225@gmail.com>
2022-05-03 17:21:23 -0300

    EventTests

--- src/test/java/br/edu/ifpb/dac/alysense/alysense/ProductTest/ProductTestService.java
+++ src/test/java/br/edu/ifpb/dac/alysense/alysense/ProductTest/ProductTestService.java
@@ -11,7 +11,10 @@ import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -28,35 +31,33 @@ import br.edu.ifpb.dac.alysense.alysense.util.ProductWithSV;
 
 
 public class ProductTestService{
-/*
+
     final ConverterService converter = new ConverterService();
  
-    private Product product;
-    private ProductDTO dto;
-    private List<Characteristic> characteristics;
-    private Characteristic characteristic;
+    final Product product= new Product();
+    final ProductDTO dto = new ProductDTO();;
+    final Set<Characteristic> characteristics = new HashSet<Characteristic>();
+    final Characteristic characteristic= new Characteristic();;
 
     
 
     @Before
     public void setUp(){
-        product = new Product();
         product.setName("bolo");
-        characteristics = new ArrayList<Characteristic>();
-        characteristic = new Characteristic();
         characteristic.setAtributes("doce");
-       // product.setCharacteristic(characteristics);
+        characteristics.add(characteristic);
+        product.setCharacteristics(characteristics);
         product.setExpirationDate(LocalDate.now());
         product.setOwner("qualquer");
     }
 
     @Before
     public void setUpDTO(){
-        dto = new ProductDTO();
         dto.setName("coxinha");
         characteristic.setAtributes("salgado");
-       // product.setCharacteristic(characteristics);
-       // dto.setCharacteristic(characteristics);
+        characteristics.add(characteristic);
+       product.setCharacteristics(characteristics);
+       dto.setCharacteristics(characteristics);
         dto.setExpirationDate(LocalDate.now());
         dto.setOwner("qualquer");
     }
@@ -75,8 +76,9 @@ public class ProductTestService{
     @Order(3)
     public void convertorTest(){
         ProductDTO dto = converter.ProductToDTO(product);
+        Iterator<Characteristic> ite = dto.getCharacteristics().iterator();
         assertAll(() -> assertEquals("bolo",dto.getName()),
-       // () -> assertEquals("doce",dto.getCharacteristic().getAtributes()),
+        () -> assertEquals("doce",ite.next().getAtributes()),
         () ->assertEquals("qualquer",dto.getOwner()),
         () -> assertEquals(LocalDate.now(),dto.getExpirationDate()));
         
@@ -87,7 +89,7 @@ public class ProductTestService{
     public void notNullTest(){
         ProductDTO dto = converter.ProductToDTO(product);
         assertAll(() -> assertNotNull(dto.getName()),
-        //() -> assertNotNull(dto.getCharacteristic()),
+        () -> assertNotNull(dto.getCharacteristics()),
         () ->assertNotNull(dto.getOwner()),
         () -> assertNotNull(dto.getExpirationDate()));
     }
@@ -95,7 +97,7 @@ public class ProductTestService{
     @Test
     @Order(4)
     public void nullTest(){
-        product = new Product();
+        Product product = new Product();
         assertNull(product.getExpirationDate());
         assertNull(product.getId());
     }
@@ -121,10 +123,10 @@ public class ProductTestService{
     @Test
     @Order(7)
     public void atribuTestsToDTO(){
-       dto = converter.ProductToDTO(product);
+       ProductDTO dto = converter.ProductToDTO(product);
        assertEquals(dto.getId(), product.getId());
        assertEquals(dto.getName(), product.getName());
-     //  assertEquals(dto.getCharacteristic(), product.getCharacteristic());
+     assertEquals(dto.getCharacteristics(), product.getCharacteristics());
        assertEquals(dto.getExpirationDate(), product.getExpirationDate());
        assertEquals(dto.getOwner(), product.getOwner());
     }
@@ -132,10 +134,10 @@ public class ProductTestService{
     @Test
     @Order(8)
     public void atribuTestsToProduct(){
-       product = converter.DTOToProduct(dto);
+       Product product = converter.DTOToProduct(dto);
        assertEquals(dto.getId(), product.getId());
        assertEquals(dto.getName(), product.getName());
-     //  assertEquals(dto.getCharacteristic(), product.getCharacteristic());
+        assertEquals(dto.getCharacteristics(), product.getCharacteristics());
        assertEquals(dto.getExpirationDate(), product.getExpirationDate());
        assertEquals(dto.getOwner(), product.getOwner());
     }
@@ -174,5 +176,5 @@ public class ProductTestService{
         
         assertFalse(prod.getSVverified());
         
-    }*/
+    }
 }
