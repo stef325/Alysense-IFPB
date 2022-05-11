3840b49e9a5482d1f6daf9aa46bf21247f7d6276
stef325 <brunastefany432@gmail.com>
2022-05-11 19:32:04 -0300

    Folders structure changed

---
+++ src/test/java/br/edu/ifpb/dac/alysense/alysense/UnitaryTests/ProductTest/ProductTestMockito.java
@@ -0,0 +1,45 @@
+package br.edu.ifpb.dac.alysense.alysense.UnitaryTests.ProductTest;
+
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.Random;
+
+@TestMethodOrder(Random.class)
+public class ProductTestMockito{
+ /*   Product product = mock(Product.class);
+
+    Characteristic chara = mock(Characteristic.class);
+
+    @Test
+    public void mockProductName(){
+        when(product.getName()).thenReturn("bolo");
+        assertNotNull(product.getName());
+
+        when(product.getName()).thenReturn("   ");
+        assertTrue(product.getName().contains(" "));
+
+        when(product.getName()).thenReturn("");
+        assertNull(product.getName());
+
+        verifyZeroInteractions(product.getName());
+    }
+
+    @Test
+    public void mockProductCharacteristics(){
+        List<Characteristic> mocked = mock(List<Characteristic>.class);
+        List<Characteristic> spy = Mockito.spy();
+    }
+
+    @Test
+    public void mockExceptiontest(){
+        when(product.getId()).thenThrow(new NullPointerException());
+        when(product.getName()).thenThrow(new NullPointerException());
+        when(product.getCharacteristics()).thenThrow(new NullPointerException());
+        when(product.getOwner()).thenThrow(new NullPointerException());
+
+        assertThrows(NullPointerException.class, () -> product.getName());
+        assertThrows(NullPointerException.class, () ->product.getId());
+        assertThrows(NullPointerException.class, ()-> product.getOwner());
+        assertThrows(NullPointerException.class,()-> product.getCharacteristics());
+    }
+    */
+}
