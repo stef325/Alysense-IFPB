f2699f3c3e267f251f8d749be129dd7b6f58680a
FilipeOBarbosa <filipeoliver225@gmail.com>
2022-06-30 19:14:15 -0300

    routerService

---
+++ client/src/services/ApiService.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const httpClient = axios.create({
+    baseURL: 'http://localhost:8081/api'
+});
+
+export default class ApiService{
+    constructor(endpoint){
+        this.endpoint = endpoint;
+    }
+
+
+    post(url, params){
+        url = this.buildUrl(url);
+        return httpClient.post(url, params);
+    }
+
+    put(url, params){
+        url = this.buildUrl(url);
+        return httpClient.put(url,params);
+    }
+
+    delete(url){
+        url = this.buildUrl(url);
+        return httpClient.delete(url);
+    }
+
+    get(url){
+        url = this.buildUrl(url);
+        return httpClient.get(url);
+    }
+
+    buildUrl(url){
+        return `${this.endpoint}${url}`;
+    }
+}
