a94ce27200ec77a7c4884ddb99f03b09b70a719d
stef325 <brunastefany432@gmail.com>
2022-07-21 00:17:50 -0300

    StorageService Created

---
+++ client/src/services/StorageService.js
@@ -0,0 +1,14 @@
+export default class StorageService{
+
+    setItem(key,value){
+        localStorage.setItem(key,JSON.stringify(value));
+    }
+
+    getItem(key){
+        return JSON.parse(localStorage.getItem(key));
+    }
+
+    removeItem(key){
+        localStorage.removeItem(key);
+    }
+}
