ba83be7f2ccc7f4dbbe94e96dc5857394e10f001
FilipeOBarbosa <filipeoliver225@gmail.com>
2022-07-23 19:06:05 -0300

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -9,19 +9,13 @@ import SessionProvider from './main/SessionProvider'
 import React from 'react';
 export default class App extends React.Component{
   render(){
-  return (
-    <div className="App">
-      <SessionProvider>
-            {/*<Route exact path="/" component={Register} />
-            <Route path="/login" component={Login} />
-            <Route path="/avaliation" component={Avaliation} />
-            <Route component={ProductUpdate} path="/updateproduct/:id" />
-            <Route component={EventUpdate} path="/EventUpdate/:id" />*/}
-              <NavBar></NavBar>
-              <AppRoutes/>
-      </SessionProvider>
-
-    </div>
-  );
-}
+    return (
+      <div className="App">
+        <SessionProvider>
+            <NavBar userType={localStorage.getItem('typeUser')}></NavBar>
+            <AppRoutes/>
+        </SessionProvider>
+      </div>
+    );
+  }
 }
